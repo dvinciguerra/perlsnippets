166dcb9dd8caae2eae47af8e5efebf9965951e1a
Daniel Vinciguerra <dan.vinciguerra@gmail.com>
2014-09-10 10:24:57 -0300

    update

---
+++ webkit/web2desktop/webkit.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Gtk2 '-init';
+use Gtk2::WebKit;
+
+# TODO:
+# - make interface better
+# - icon buttons
+# - make it object oriented!?
+
+
+my $mw = Gtk2::Window->new;
+$mw->set_title("GtkPerl Mini Webkit");
+$mw->set_default_size( '800', '600' );
+$mw->signal_connect( 'delete_event' => sub { Gtk2->quit; } );
+
+my $main_table    = Gtk2::Table->new( 4, 2 );
+my $scroll_window = Gtk2::ScrolledWindow->new;
+my $browser       = Gtk2::WebKit::WebView->new;
+
+my $back_button    = Gtk2::Button->new_from_stock('gtk-go-back');
+my $forward_button = Gtk2::Button->new_from_stock('gtk-go-forward');
+my $reload_button  = Gtk2::Button->new_from_stock('gtk-refresh');
+my $url_entry      = Gtk2::Entry->new;
+$url_entry->set_text('http://');
+
+$main_table->attach( $back_button, 0, 1, 0, 1, [ 'expand', 'fill' ],
+    'expand', 10, 10 );
+
+$main_table->attach( $forward_button, 1, 2, 0, 1, [ 'expand', 'fill' ],
+    'expand', 10, 10 );
+
+$main_table->attach( $reload_button, 2, 3, 0, 1, [ 'expand', 'fill' ],
+    'expand', 10, 10 );
+
+$main_table->attach( $url_entry, 3, 4, 0, 1, [ 'expand', 'fill' ],
+    'expand', 10, 10 );
+
+$main_table->attach(
+    $scroll_window, 0, 4, 1, 2,
+    [ 'expand', 'fill' ],
+    [ 'expand', 'fill' ],
+    10, 10
+);
+
+$scroll_window->set_size_request( '-1', '700' );
+
+$mw->add($main_table);
+$scroll_window->add($browser);
+
+$url_entry->signal_connect(
+    'activate' => sub {
+        my ( $widget, $event ) = @_;
+        open_url( $widget, $event, $browser );
+    }
+);
+
+$reload_button->signal_connect(
+    'clicked' => sub {
+        my ( $widget, $event ) = @_;
+        reload_url( $widget, $event, $browser );
+    }
+);
+
+$back_button->signal_connect(
+    'clicked' => sub {
+        my ( $widget, $event ) = @_;
+        back_url( $widget, $event, $browser );
+    }
+);
+
+$forward_button->signal_connect(
+    'clicked' => sub {
+        my ( $widget, $event ) = @_;
+        forward_url( $widget, $event, $browser );
+    }
+);
+
+$mw->show_all;
+Gtk2->main;
+
+sub open_url {
+    my ( $widget, $event, $browser ) = @_;
+    my $url = $widget->get_text;
+    
+    $url = "http://". $url unless $url =~ /^https?:\/\//;
+    $widget->set_text($url);
+
+    $browser->open($url);
+}
+
+sub reload_url {
+    my ( $widget, $event, $browser ) = @_;
+    $browser->reload;
+}
+
+sub back_url {
+    my ( $widget, $event, $browser ) = @_;
+    $browser->go_back;
+}
+
+sub forward_url {
+    my ( $widget, $event, $browser ) = @_;
+    $browser->go_forward;
+}
+
